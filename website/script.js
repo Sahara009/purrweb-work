dbcd3677e8f4730d12b3075656ce7f9574fb0767
Sahara009 <148674490+Sahara009@users.noreply.github.com>
2024-11-21 14:36:15 +0300

    add: refactoring styles, cookies block

--- website/script.js
+++ website/script.js
@@ -9,3 +9,33 @@ document.querySelector(".header__burger").addEventListener("click", () => {
 document.querySelector(".popup__closeBtn").addEventListener("click", () => {
   document.querySelector(".popup").style.display = "none";
 });
+document.querySelector(".cookies__closeBtn").addEventListener("click", () => {
+  document.querySelector(".cookies").style.display = "none";
+});
+document.querySelector(".cookies__accept").addEventListener("click", () => {
+  document.querySelector(".cookies").style.display = "none";
+});
+document.querySelector(".cookies__decline").addEventListener("click", () => {
+  document.querySelector(".cookies").style.display = "none";
+});
+document.querySelector(".success__closeBtn").addEventListener("click", () => {
+  document.querySelector(".success").style.display = "none";
+});
+document.querySelector(".success__button").addEventListener("click", () => {
+  document.querySelector(".success").style.display = "none";
+});
+
+const form = document.getElementById("contactForm");
+const popup = document.getElementById("popup");
+const successPopup = document.querySelector(".success");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  if (form.checkValidity()) {
+    popup.style.display = "none";
+    successPopup.style.display = "block";
+  } else {
+    alert("Пожалуйста, заполните все поля корректно.");
+  }
+});
